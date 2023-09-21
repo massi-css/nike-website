05fd321b5104288f825ee297a36c022cc4e817f9
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2023-09-21 23:49:20 +0100

    updated components

--- nike/src/App.jsx
+++ nike/src/App.jsx
@@ -1,17 +1,24 @@
 import Hero from "./components/Hero";
 import PopularProducts from "./components/PopularProducts"
+import SuperQuality from "./components/SuperQuality"
+import Services from "./components/Services"
+import SpecialOffer from "./components/SpecialOffer"
+import CostumerReviews from "./components/CostumerReviews"
+import Subscribe from "./components/Subscribe"
+import Footer from "./components/Footer"
+
 
 const App = () => (
   <main className="relative">
     Nav
-    <section className="xl:padding-1 wide: padding-r padding-b"><Hero></Hero></section>
-    <section className="padding"> <PopularProducts></PopularProducts> </section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className="bg-pale-blue padding">CustumerReviews</section>
-    <section className="padding-x sm:py-32 px-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    <section className="xl:padding-1 wide: padding-r padding-b"><Hero/></section>
+    <section className="padding"> <PopularProducts/></section>
+    <section className="padding"><SuperQuality/></section>
+    <section className="padding-x py-10"><Services/></section>
+    <section className="padding"><SpecialOffer/></section>
+    <section className="bg-pale-blue padding"><CostumerReviews/></section>
+    <section className="padding-x sm:py-32 px-16 w-full"><Subscribe/></section>
+    <section className="bg-black padding-x padding-t pb-8"><Footer/></section>
   </main>
 );
 
