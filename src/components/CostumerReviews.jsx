8f6dddf6b50b64adb4a2e6a70326cfbb1984e215
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2023-09-24 00:34:20 +0100

    styled customerReview section

--- src/components/CostumerReviews.jsx
+++ src/components/CostumerReviews.jsx
@@ -1,7 +1,31 @@
+import { reviews } from "../constants";
+import ReviewCard from "./ReviewCard";
+
 const CostumerReviews = () => {
   return (
-    <div>CostumerReviews</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red">Custumers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experience with us.
+      </p>
+      <div className="mt-24 flex flex-1 flex-wrap justify-evenly items-center max-lgg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default CostumerReviews
+export default CostumerReviews;
